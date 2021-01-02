08d96130ab57973719e8c81d641892bb594e14ca
MOHOAzure <timecrutramer@gmail.com>
2021-01-03 00:52:30 +0800

    save detection result into avi

--- video_recognition.py
+++ video_recognition.py
@@ -1,5 +1,5 @@
 '''
-This script is to apply trained model to detect guns in a video clip
+This script applys a trained model to detect guns in a video clip and saved the detection video
 '''
 
 import numpy as np
@@ -7,33 +7,49 @@ import cv2
 import imutils
 import datetime
 
+# cascade
 gun_cascade = cv2.CascadeClassifier('cascade.xml')
-camera = cv2.VideoCapture('data/input.webm')
+
+# input video
+input_video = cv2.VideoCapture('data/input.webm')
+
+# save video - Set resolutions, convert them from float to integer. 
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+fps= 60.0
+frame_width = int(input_video.get(3)) 
+frame_height = int(input_video.get(4))
+size = (frame_width, frame_height)
+out = cv2.VideoWriter('output.avi', fourcc, fps, size) 
+
+# out = cv2.VideoWriter('output.avi',fourcc, 30.0, (640,360))
 
 # initialize the first frame in the video stream
 firstFrame = None
 
-# loop over the frames of the video
-
+# flag if any gun exists
 gun_exist = False
 
+# check whether input video is opened previously or not 
+if not input_video.isOpened():  
+    print("Error reading video file") 
+
+# loop over the frames of the video    
 while True:
-    (grabbed, frame) = camera.read()
+    (grabbed, frame) = input_video.read()
 
     # if the frame could not be grabbed, then we have reached the end of the video
     if not grabbed:
         break
-
+            
     # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=500)
+    # frame = imutils.resize(frame, width=500)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
     
-    gun = gun_cascade.detectMultiScale(gray, 1.3, 5, minSize = (100, 100))
-    
-    if len(gun) > 0:
-        gun_exist = True
-        
+    # detect & rect gun
+    gun = gun_cascade.detectMultiScale(gray, 1.3, 5, minSize = (100, 100))    
+    if not gun_exist and len(gun)> 0:
+        gun_exist = True        
     for (x,y,w,h) in gun:
         frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
@@ -47,18 +63,27 @@ while True:
     # draw the text and timestamp on the frame
     cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
                     (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+                    
+    # write the output video
+    out.write(frame)
 
-    # show the frame and record if the user presses a key
-    cv2.imshow("Security Feed", frame)
-    key = cv2.waitKey(1) & 0xFF
+    # show the frame
+    cv2.imshow("Security Feed", frame)    
+    
+    
+    # quit by 'q' key
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
+# finish detection
 if gun_exist:
     print("guns detected")
 else:
     print("guns NOT detected")
 
-# cleanup the camera and close any open windows
-camera.release()
+# cleanup the input_video, output stream, and close any open windows
+input_video.release()
+out.release()
 cv2.destroyAllWindows()
 
 
